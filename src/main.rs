2d4c10f0592ed7e7e49350d7c27a8320332a659a
koerriva <rivakoer@gmail.com>
2020-06-24 09:06:22 +0800

    Ray Tracing in a Weekend; chapter12:最终效果-05

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ use std::time::{Duration, SystemTime};
 use std::sync::mpsc::{channel, Sender, Receiver};
 
 pub const ASPECT_RATIO:Float = 16.0/9.0;
-pub const IMAGE_WIDTH:i32 = 1280;
+pub const IMAGE_WIDTH:i32 = 1920;
 pub const IMAGE_HEIGHT:i32 = (IMAGE_WIDTH as Float / ASPECT_RATIO) as i32;
 pub const BUFFER_SIZE:usize = (IMAGE_WIDTH * IMAGE_HEIGHT * 3) as usize;
-pub const NS:i32 = 200;
+pub const NS:i32 = 64*4;//SSAA采样
 
 macro_rules! lambertian {
 	($r:expr,$g:expr,$b:expr) => {
@@ -129,5 +129,5 @@ fn main() {
 	println!("Done. {:?},Total {}",end_time,end_time.duration_since(start_time).unwrap().as_secs());
 
 	let buffer = frame.get_raw_buffer();
-	image::save_buffer("images/12-7.png", &buffer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, ColorType::Rgb8).unwrap()
+	image::save_buffer("images/12-9.png", &buffer, IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, ColorType::Rgb8).unwrap()
 }
